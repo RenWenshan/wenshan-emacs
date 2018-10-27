f7953563e5800550adaecd2905e2ac1e99ec31b6
Wenshan Ren <renws1990@gmail.com>
2018-10-28 09:02:04 +1100

    Add some goodies:
    - super-save
    - y-or-n instead yes-or-no
    - delete trailing whitespaces on saving

--- site-lisp/config/init-startup.el
+++ site-lisp/config/init-startup.el
@@ -6,6 +6,22 @@
 ;; get environment variables such as $PATH from the shell
 (require 'cache-path-from-shell)
 
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; delete trailing whitespace on saving
+(add-hook 'before-save-hook
+          (lambda ()
+            (delete-trailing-whitespace)))
+
+;; use super-save instead of built-in auto-save
+(setq auto-save-default nil)
+(setq make-backup-files nil)   ; no need for backups since auto-saving
+
+(require 'super-save)
+(super-save-mode +1)
+(setq super-save-auto-save-when-idle t)
+
 ;; start emacs server for fast emacsclient startup
 (server-start)
 
