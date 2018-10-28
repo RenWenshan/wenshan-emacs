4734ac094245473819d875158d30e0e9c693362a
Wenshan Ren <renws1990@gmail.com>
2018-10-29 00:26:06 +1100

    Add sdcv for EN<->CN dictionary lookup and restructured init-zh-cn.

---
+++ site-lisp/config/init-zh-cn/init-sdcv.el
@@ -0,0 +1,34 @@
+(require 'sdcv)
+
+(setq sdcv-say-word-p nil) 		; do not pronounce
+
+(setq sdcv-dictionary-simple-list
+      '("懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        "KDic11万英汉词典"))
+
+(setq sdcv-dictionary-complete-list
+      '(
+        "懒虫简明英汉词典"
+        "英汉汉英专业词典"
+        "XDICT英汉辞典"
+        "stardict1.3英汉辞典"
+        "WordNet"
+        "XDICT汉英辞典"
+        "Jargon"
+        "懒虫简明汉英词典"
+        "FOLDOC"
+        "新世纪英汉科技大词典"
+        "KDic11万英汉词典"
+        "朗道汉英字典5.0"
+        "CDICT5英汉辞典"
+        "新世纪汉英科技大词典"
+        "牛津英汉双解美化版"
+        "21世纪双语科技词典"
+        "quick_eng-zh_CN"
+        ))
+
+(setq sdcv-dictionary-data-dir (concat (file-name-as-directory wenshan-emacs-data-dir)
+				       "/sdcv"))
+
+(provide 'init-sdcv)
---
+++ site-lisp/config/init-zh-cn/init-zh-cn.el
@@ -0,0 +1,4 @@
+(require 'init-pyim)
+(require 'init-sdcv)
+
+(provide 'init-zh-cn)
