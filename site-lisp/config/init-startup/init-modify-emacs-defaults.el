e61c8ce2f9ba74412e6044201c2182bffba65b42
Wenshan Ren <renws1990@gmail.com>
2018-11-05 12:34:59 +1100

    Stop cursor from blinking.

--- site-lisp/config/init-startup/init-modify-emacs-defaults.el
+++ site-lisp/config/init-startup/init-modify-emacs-defaults.el
@@ -1,6 +1,7 @@
 (tool-bar-mode -1)			; hide tool bar for more space
 (menu-bar-mode -1)			; hide menu bar for more space
 (scroll-bar-mode -1)			; hide scroll bar for more space
+(blink-cursor-mode -1)			; stop cursor from blinking
 (fset 'yes-or-no-p 'y-or-n-p)		; "y or n" instead of "yes or no"
 (setq ring-bell-function 'ignore)	; prevent beep
 (setq epa-pinentry-mode 'loopback)	; type epa password inside emacs
