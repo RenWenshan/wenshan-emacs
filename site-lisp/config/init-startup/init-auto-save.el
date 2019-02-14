50a85afbc0920cd906388df44455524c3400c099
Ren, Wenshan <Wenshan.Ren@team.telstra.com>
2019-02-15 10:24:36 +1100

    Replaced auto-save with super-save.

--- site-lisp/config/init-startup/init-auto-save.el
+++ site-lisp/config/init-startup/init-auto-save.el
@@ -1,10 +1,9 @@
-(setq auto-save-default nil)		; disable emacs built in auto-save
-(setq make-backup-files nil)		; no need for backups with auto-save
+(setq auto-save-default nil)		; disable emacs built-in auto-save
+(setq make-backup-files nil)		; disable backups
 
-(require 'auto-save)
-(setq auto-save-idle 1)			; save if Emacs is idle for 1 second
-(setq auto-save-silent t)
-(setq auto-save-delete-trailing-whitespace t) ; delete trailing spaces except for the current line
-(auto-save-enable)
+(require 'super-save)                   ; save on switching buffers, losing focus, etc
+(setq super-save-remote-files nil)      ; disable super-save for remote files
+(super-save-mode t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (provide 'init-auto-save)
