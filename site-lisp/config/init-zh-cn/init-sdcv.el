125de870c08f5ed771b278e5739ea2a9b372d06d
Wenshan Ren <renws1990@gmail.com>
2019-09-09 14:59:03 +1000

    Remove unused sdcv dictionaries.

--- site-lisp/config/init-zh-cn/init-sdcv.el
+++ site-lisp/config/init-zh-cn/init-sdcv.el
@@ -7,27 +7,6 @@
         "懒虫简明汉英词典"
         "KDic11万英汉词典"))
 
-(setq sdcv-dictionary-complete-list
-      '(
-        "懒虫简明英汉词典"
-        "英汉汉英专业词典"
-        "XDICT英汉辞典"
-        "stardict1.3英汉辞典"
-        "WordNet"
-        "XDICT汉英辞典"
-        "Jargon"
-        "懒虫简明汉英词典"
-        "FOLDOC"
-        "新世纪英汉科技大词典"
-        "KDic11万英汉词典"
-        "朗道汉英字典5.0"
-        "CDICT5英汉辞典"
-        "新世纪汉英科技大词典"
-        "牛津英汉双解美化版"
-        "21世纪双语科技词典"
-        "quick_eng-zh_CN"
-        ))
-
 (setq sdcv-dictionary-data-dir (concat wenshan-emacs-data-dir "/sdcv"))
 
 (provide 'init-sdcv)
