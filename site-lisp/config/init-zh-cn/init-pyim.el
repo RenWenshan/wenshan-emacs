762c40ee9a07a6259c75e486456988696b0f8fe0
Wenshan Ren <renws1990@gmail.com>
2019-09-09 15:09:09 +1000

    Adjust pyim: disable fuzzy pinyin.

--- site-lisp/config/init-zh-cn/init-pyim.el
+++ site-lisp/config/init-zh-cn/init-pyim.el
@@ -8,6 +8,7 @@
 (pyim-isearch-mode 1)                           ; 开启拼音搜索功能
 (setq pyim-page-tooltip 'posframe)      ; 默认使用 pupup-el 来绘制选词框，改用 'posframe, 速度很快并且菜单不会变形
 (setq pyim-page-length 9)               ; 选词框显示9个候选词
+(setq pyim-fuzzy-pinyin-alist nil)      ; 不使用模糊拼音
 (add-hook 'emacs-startup-hook #'(lambda () (pyim-restart-1 t))) ; 让 Emacs 启动时自动加载 pyim
 
 (provide 'init-pyim)
