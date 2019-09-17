5a22900f785fbcac6add4f3bcc09ce6e3d98e3c2
Wenshan Ren <renws1990@gmail.com>
2019-09-17 10:25:22 +1000

    Enable lsp-mode for c++-mode and add proper c sytling

--- site-lisp/config/init-hacking/init-c.el
+++ site-lisp/config/init-hacking/init-c.el
@@ -1,3 +1,15 @@
 (add-hook 'c-mode-hook #'lsp)
+(add-hook 'c++-mode-hook #'lsp)
+
+;; fix indentation https://stackoverflow.com/posts/26051450/revisions
+(defun fix-c-indent-offset-according-to-syntax-context (key val)
+  (setq c-offsets-alist (delq (assoc key c-offsets-alist) c-offsets-alist))
+  (add-to-list 'c-offsets-alist '(key . val)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode)
+              (fix-c-indent-offset-according-to-syntax-context 'substatement-open 0))
+            ))
 
 (provide 'init-c)
