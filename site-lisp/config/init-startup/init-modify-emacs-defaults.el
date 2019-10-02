6bb471b6fb2699cef774738bef05884bf5464f2c
Wenshan Ren <renws1990@gmail.com>
2019-10-02 17:10:38 +1000

    Enable built-in show-paren-mode

--- site-lisp/config/init-startup/init-modify-emacs-defaults.el
+++ site-lisp/config/init-startup/init-modify-emacs-defaults.el
@@ -2,6 +2,7 @@
 (tool-bar-mode -1)			; hide tool bar for more space
 (menu-bar-mode -1)			; hide menu bar for more space
 (scroll-bar-mode -1)			; hide scroll bar for more space
+(show-paren-mode 1)                     ; highlight matching parenthesis
 (setq scroll-step 3)                    ; smooth scroll
 (global-hl-line-mode t)                 ; highlight current line globally
 (blink-cursor-mode 1)			; stop cursor from blinking
