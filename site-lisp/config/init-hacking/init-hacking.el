0ebd721d26fd9d986186a872a8f77f65f11ff889
Wenshan Ren <renws1990@gmail.com>
2022-09-10 08:32:07 +1000

    Clean up hacking related stuff

--- site-lisp/config/init-hacking/init-hacking.el
+++ site-lisp/config/init-hacking/init-hacking.el
@@ -1,18 +1,8 @@
 (use-package init-parenthesis-completion)
 (use-package init-template-system)
-(use-package init-lsp)
-
-(use-package flycheck
-  :commands (flycheck-mode)
-  :config
-  (require 'pkg-info))
 (use-package init-code-folding)
 (use-package init-code-sending)
 (use-package init-indentation-highlight)
 (use-package init-line-numbering)
-
-(use-package init-python)
-(use-package init-javascript)
-(use-package init-c)
 
 (provide 'init-hacking)
--- site-lisp/config/init-hacking/init-python.el
+++
@@ -1,3 +0,0 @@
-(setq python-indent-offset 2)
-
-(provide 'init-python)
