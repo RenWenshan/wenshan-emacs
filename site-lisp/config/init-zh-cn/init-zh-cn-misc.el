86092dc11ab74db9b5fc5ad03a938eb9d86e8b6d
Wenshan Ren <renws1990@gmail.com>
2022-10-27 10:43:07 +1100

    Enable pinyin search keybindings and remove unused extensions

--- site-lisp/config/init-zh-cn/init-zh-cn-misc.el
+++ site-lisp/config/init-zh-cn/init-zh-cn-misc.el
@@ -1,6 +1,6 @@
-;; (use-package pinyin-search
-;;   :ensure t
-;;   :bind (("s-s" . 'pinyin-search)
-;;          ("M-s-s" . 'pinyin-search-backward)))
+(use-package pinyin-search
+  :ensure t
+  :bind (("s-s" . 'pinyin-search)
+         ("M-s-s" . 'pinyin-search-backward)))
 
 (provide 'init-zh-cn-misc)
